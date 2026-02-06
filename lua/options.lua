73ad1a04c86f0f9fa554dabf67e9ec31c6f3a0a1
Kencho5 <giokenchadze@gmail.com>
2026-02-06 12:56:34 +0400

    add: angular file detection

--- lua/options.lua
+++ lua/options.lua
@@ -9,4 +9,16 @@ vim.opt.guicursor = "i:block" --  set cursor of insert mode as block.
 vim.cmd "set guicursor=a:blinkon100"
 vim.g.nvim_tree_show_hidden = 1
 
--- vim.cmd "autocmd BufNewFile,BufRead *.html set ft=html"
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.component.html", "*.template.html" },
+  callback = function()
+    vim.bo.filetype = "htmlangular"
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*/component/**/*.html",
+  callback = function()
+    vim.bo.filetype = "htmlangular"
+  end,
+})
